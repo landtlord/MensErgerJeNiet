31745ed4b4710a67f1b43326495de56fb27c07e5
landtlord <andy.landtsheer@pandora.be>
2021-02-18 20:06:27 +0100

    adding board places array

--- src/app/services/play-field.service.ts
+++ src/app/services/play-field.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {PlayFieldPlace} from '../model/play-field-place';
 import {Pawn} from '../model/pawn/pawn';
 import {Color} from '../model/pawn/color.enum';
 import {Coordinate} from '../model/pawn/coordinate';
@@ -57,29 +56,51 @@ export class PlayFieldService {
   }
 
 
+  private readonly RED_START = this.coordinates[0];
+  private readonly BLEU_START = this.coordinates[10];
+  private readonly GREEN_START = this.coordinates[20];
+  private readonly YELLOW_START = this.coordinates[30];
+
   initiate(): void {
-    this.pawns.push(new Pawn(Color.YELLOW, new Coordinate(4, 10), 'Yellow1'));
-    this.pawns.push(new Pawn(Color.YELLOW, new Coordinate(0, 10), 'Yellow2'));
-    this.pawns.push(new Pawn(Color.YELLOW, new Coordinate(1, 9), 'Yellow3'));
-    this.pawns.push(new Pawn(Color.YELLOW, new Coordinate(1, 10), 'Yellow4'));
-    this.pawns.push(new Pawn(Color.RED, new Coordinate(0, 4), 'Red1'));
+    this.pawns.push(new Pawn(Color.RED, new Coordinate(0, 0), 'Red1'));
     this.pawns.push(new Pawn(Color.RED, new Coordinate(0, 1), 'Red2'));
     this.pawns.push(new Pawn(Color.RED, new Coordinate(1, 0), 'Red3'));
     this.pawns.push(new Pawn(Color.RED, new Coordinate(1, 1), 'Red4'));
-    this.pawns.push(new Pawn(Color.BLEU, new Coordinate(6, 0), 'Bleu1'));
+    this.pawns.push(new Pawn(Color.BLEU, new Coordinate(9, 0), 'Bleu1'));
     this.pawns.push(new Pawn(Color.BLEU, new Coordinate(9, 1), 'Bleu2'));
     this.pawns.push(new Pawn(Color.BLEU, new Coordinate(10, 0), 'Bleu3'));
     this.pawns.push(new Pawn(Color.BLEU, new Coordinate(10, 1), 'Bleu4'));
-    this.pawns.push(new Pawn(Color.GREEN, new Coordinate(10, 6), 'Green1'));
-    this.pawns.push(new Pawn(Color.GREEN, new Coordinate(10, 9), 'Green2'));
-    this.pawns.push(new Pawn(Color.GREEN, new Coordinate(9, 10), 'Green3'));
-    this.pawns.push(new Pawn(Color.GREEN, new Coordinate(9, 9), 'Green4'));
+    this.pawns.push(new Pawn(Color.GREEN, new Coordinate(9, 9), 'Green1'));
+    this.pawns.push(new Pawn(Color.GREEN, new Coordinate(9, 10), 'Green2'));
+    this.pawns.push(new Pawn(Color.GREEN, new Coordinate(10, 9), 'Green3'));
+    this.pawns.push(new Pawn(Color.GREEN, new Coordinate(10, 10), 'Green4'));
+    this.pawns.push(new Pawn(Color.YELLOW, new Coordinate(0, 9), 'Yellow1'));
+    this.pawns.push(new Pawn(Color.YELLOW, new Coordinate(0, 10), 'Yellow2'));
+    this.pawns.push(new Pawn(Color.YELLOW, new Coordinate(1, 9), 'Yellow3'));
+    this.pawns.push(new Pawn(Color.YELLOW, new Coordinate(1, 10), 'Yellow4'));
   }
 
   movePawn(pawn: Pawn, dice: number): void {
     let index = this.coordinates.findIndex(coordinate => coordinate === pawn.coordinate);
-    index = (index + dice) % 40;
-    pawn.coordinate = this.coordinates[index];
+    if (index === -1) {
+      switch (pawn.color) {
+        case Color.RED:
+          pawn.coordinate = this.RED_START;
+          break;
+        case Color.BLEU:
+          pawn.coordinate = this.BLEU_START;
+          break;
+        case Color.GREEN:
+          pawn.coordinate = this.GREEN_START;
+          break;
+        case Color.YELLOW:
+          pawn.coordinate = this.YELLOW_START;
+          break;
+      }
+    } else {
+      index = (index + dice) % 40;
+      pawn.coordinate = this.coordinates[index];
+    }
   }
 
   getBoard(): Pawn[] {
